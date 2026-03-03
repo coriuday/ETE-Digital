1fa377a7b50d9d41dafeb80198d4b0efbd863b7a
Uday Kumar Kori <coriuday.18@gmail.com>
2026-03-03 14:23:43 +0530

    feat: Add `start_servers.py` to concurrently launch frontend and backend development servers.

---
+++ ete-digital/start_servers.py
@@ -0,0 +1,9 @@
+import subprocess
+
+f = open('d:/all_projects/ind/ete-digital/frontend_log.txt', 'w')
+p1 = subprocess.Popen('npm run dev', shell=True, cwd='d:/all_projects/ind/ete-digital/frontend', stdout=f, stderr=subprocess.STDOUT)
+
+b = open('d:/all_projects/ind/ete-digital/backend_log.txt', 'w')
+p2 = subprocess.Popen('uvicorn app.main:app --host 0.0.0.0 --port 8000', shell=True, cwd='d:/all_projects/ind/ete-digital/backend', stdout=b, stderr=subprocess.STDOUT)
+
+print("Started with PIDs:", p1.pid, p2.pid)
