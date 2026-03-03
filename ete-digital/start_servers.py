6a6eefa87639e257c84801d972662c91408c272f
Uday Kumar Kori <coriuday.18@gmail.com>
2026-03-03 14:24:05 +0530

    feat: Add script to concurrently start frontend and backend development servers.

--- ete-digital/start_servers.py
+++ ete-digital/start_servers.py
@@ -7,3 +7,6 @@ b = open('d:/all_projects/ind/ete-digital/backend_log.txt', 'w')
 p2 = subprocess.Popen('uvicorn app.main:app --host 0.0.0.0 --port 8000', shell=True, cwd='d:/all_projects/ind/ete-digital/backend', stdout=b, stderr=subprocess.STDOUT)
 
 print("Started with PIDs:", p1.pid, p2.pid)
+import time
+while True:
+    time.sleep(1)
