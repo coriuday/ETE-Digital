486a3275b943abd950b8ca56a99a9e665cde04d8
Uday Kumar Kori <coriuday.18@gmail.com>
2026-03-20 20:44:10 +0530

    feat: Add initial Vite configuration for the frontend project.

--- ete-digital/frontend/vite.config.ts
+++ ete-digital/frontend/vite.config.ts
@@ -23,5 +23,19 @@ export default defineConfig({
     build: {
         outDir: 'dist',
         sourcemap: true,
+        chunkSizeWarningLimit: 1000,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        const segments = id.toString().split('node_modules/')[1].split('/');
+                        if (segments[0].startsWith('@')) {
+                            return `${segments[0]}/${segments[1]}`;
+                        }
+                        return segments[0];
+                    }
+                }
+            }
+        }
     },
 })
