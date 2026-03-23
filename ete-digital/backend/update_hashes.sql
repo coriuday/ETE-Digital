3bd89919d725f2e1c4eaf7d5f7ce47184c2f9f19
ashuisalluneed <zadavashish1995@gmail.com>
2026-03-23 17:06:49 +0530

    feat: Generate Argon2 password hashes for test accounts and provide an SQL update script.

---
+++ ete-digital/backend/update_hashes.sql
@@ -0,0 +1,2 @@
+UPDATE users SET password_hash = '$argon2id$v=19$m=65536,t=3,p=4$MMa4935vbc2ZU+r9fy/F2A$FHnOOZZDaouOsPbwbKTjKLfdhkCTECI/lo2Y43jOmjo' WHERE email = 'admin@etedigital.com';
+UPDATE users SET password_hash = '$argon2id$v=19$m=65536,t=3,p=4$JqSUcs65NwZAqFUqBQAg5A$PTqW078gVcRwZKcEHMq8MhwqxFytqSdM/7f5o6R7MFI' WHERE email = 'priya.sharma.tech@gmail.com';
